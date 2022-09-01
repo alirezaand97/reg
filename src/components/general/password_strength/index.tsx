cfa3aa3e023fb08bda792289a38c6372d9ebb983
alirezaand97 <alirezaand97@gmail.com>
2022-09-01 23:30:49 +0430

    fix password regex

--- src/components/general/password_strength/index.tsx
+++ src/components/general/password_strength/index.tsx
@@ -5,6 +5,7 @@ import {
   specialCharRegex,
   upperCaseRegex,
 } from "@/constant/regex_format";
+import { Transition } from "@headlessui/react";
 import { useEffect, useState } from "react";
 
 interface Props {
@@ -15,58 +16,59 @@ const PasswordStrength = (props: Props) => {
   const { password, show } = props;
   const [isValid, setIsValid] = useState(false);
   useEffect(() => {
-    if (checkStrength()) {
-      setIsValid(true);
-    } else {
-      setIsValid(false);
-    }
+    checkStrength() ? setIsValid(true) : setIsValid(false);
   }, [password]);
 
   const passwordChecklist = {
     uppercase: {
-      isValid: upperCaseRegex.test(password),
-      message: "حداقل یک حرف بزرگ",
-    },
-    lowercase: {
-      isValid: lowerCaseRegex.test(password),
-      message: "حداقل یک حرف کوچک",
+      isValid: upperCaseRegex.test(password) && lowerCaseRegex.test(password),
+      message: "شامل حروف بزرگ و کوچک",
     },
     number: {
       isValid: numberRegex.test(password),
-      message: "حداقل یک عدد",
+      message: "شامل عدد",
     },
     spechialChar: {
       isValid: specialCharRegex.test(password),
-      message: "حداقل یک کاراکتر خاص",
+      message: "شامل علائم وِیژه",
     },
     length: {
       isValid: password.length >= 8 && password.length <= 20,
-      message: "طول بین 8 تا 20 کاراکتر",
+      message: "حداقل 8 کاراکتر",
     },
   };
 
   const checkStrength = () => {
     return Object.values(passwordChecklist).every((item) => item.isValid);
   };
-  if (!show) return null;
   return (
-    <div className="mt-1">
-      {!isValid
-        ? Object.values(passwordChecklist).map((item,index) => (
-            <div className="text-xs" key={index}>
-              {
-                <div
-                  className={`${
-                    item.isValid ? "text-teal-500" : "text-red-500"
-                  }`}
-                >
-                  {item.message}
-                </div>
-              }
-            </div>
-          ))
-        : ""}
-    </div>
+    <Transition
+      show={show}
+      enter="transition-opacity ease-linear duration-300"
+      enterFrom="opacity-0"
+      enterTo="opacity-100"
+      leave="transition-opacity ease-linear duration-300"
+      leaveFrom="opacity-100"
+      leaveTo="opacity-0"
+    >
+      <div className="mt-1">
+        {!isValid
+          ? Object.values(passwordChecklist).map((item, index) => (
+              <div className="text-xs" key={index}>
+                {
+                  <div
+                    className={`${
+                      item.isValid ? "text-teal-500" : "text-red-500"
+                    }`}
+                  >
+                    {item.message}
+                  </div>
+                }
+              </div>
+            ))
+          : ""}
+      </div>
+    </Transition>
   );
 };
 
